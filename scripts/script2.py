7a7a6e07596403e6dc801c592195f30e57c63bf0
juankarmjackson <51057522+juankarmjackson@users.noreply.github.com>
2025-04-01 11:46:02 +0200

    Update script2.py

--- scripts/script2.py
+++ scripts/script2.py
@@ -7,6 +7,8 @@ from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
 from selenium.webdriver.chrome.service import Service
 from webdriver_manager.chrome import ChromeDriverManager
+driver = webdriver.Chrome(options=options)
+
 
 def login():
     print("🔐 Iniciando sesión en Feliu Badaló...")
